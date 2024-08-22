bea6efbf477b86b8749cab7fcce04d2c0732cd8a
Marc <122339963+MarcusGitty@users.noreply.github.com>
2024-08-22 11:15:38 +0800

    Initial commit

---
+++ marcusyap.com/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import myPic from "./assets/ProfilePic.jpg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src= {myPic} className="App-logo" alt="logo" />
+        <p>
+          Hi I'm Marcus Yap and I'm a software engineer. I'm currently working on my personal website. Stay tuned!
+        </p>
+
+        <p>
+          Check out my LinkedIn profile!
+          
+        </p>
+
+        <p>
+          Website Created on 21 August 2024
+        </p>
+        <a
+          className="LinkedIn"
+          href="https://www.linkedin.com/in/marcus~yap/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
