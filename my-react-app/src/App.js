4a6286fa6964f9b2c0f7bb9823a33a810dc8d87a
Marc <122339963+MarcusGitty@users.noreply.github.com>
2024-08-22 13:38:45 +0800

    Edit Landing Page

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,21 +1,30 @@
-import logo from './logo.svg';
+import profilepic from './asset/ProfilePic.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={profilepic} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi I'm Marcus Yap and I'm a software engineer. I'm currently working on my personal website. Stay tuned!
+        </p>
+
+        <p>
+          Check out my LinkedIn profile!
+          
+        </p>
+
+        <p>
+          Website Created on 21 August 2024
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="LinkedIn"
+          href="https://www.linkedin.com/in/marcus~yap/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LinkedIn
         </a>
       </header>
     </div>
